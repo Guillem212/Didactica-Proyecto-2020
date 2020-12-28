7012529894fb80322c510b1d0d86f494316fe0eb
MiguelFMD <al364481@uji.es>
2020-12-28 16:01:41 +0000

    menu principal (a falta de boton de continuar)

---
+++ Didactica-Proyecto/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator transition;
+    public void StartGame(string sceneName) //El nombre se le pasa desde el botón
+    {
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
