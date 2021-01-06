6438800b97ff9db693cc52ff383cdb9f26157808
riki8245 <56740262+riki8245@users.noreply.github.com>
2021-01-06 03:44:57 +0100

    mini changes
    es imposible controlar bien los saltos de linea con ciertas cosas lo hare by hand

--- Didactica-Proyecto/Assets/FillChatWithMessages.cs
+++ Didactica-Proyecto/Assets/FillChatWithMessages.cs
@@ -35,21 +35,37 @@ public class FillChatWithMessages : MonoBehaviour
             if (item.Value.person_name.Equals(name))
             {
                 foreach(var msginChat in item.Value.messages)
-                {                 
-                    int cont_sl = msginChat.Value.text.Length < 29? 0 : msginChat.Value.text.Length/29 + 1;                    
-
+                {
+                    int cont_sl = 0;//msginChat.Value.text.Length < 29? 0  : (int)(((float)msginChat.Value.text.Length / 29) - (((float)msginChat.Value.text.Length / 29) % 1));
+                    int cont_ch = 0;
+                    int cont_p = 0;
+                    foreach(char c in msginChat.Value.text)
+                    {
+                        cont_ch++;
+                        if (!c.Equals(" ")) cont_p++;
+                        else if (c.Equals(" ")) cont_p = 0;
+                        if (cont_ch > 29)
+                        {
+                            cont_ch = cont_p < 29?cont_p : 0;
+                            cont_sl++;
+                        }
+                    }
                     received_msg.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = msginChat.Value.text;
                     GameObject msg = Instantiate(received_msg, transform);
 
                     RectTransform rt = msg.transform.Find("bg_image").GetComponent<RectTransform>();
-                    msg.transform.Find("bg_image").GetComponent<RectTransform>().sizeDelta = new Vector2(Mathf.Clamp(800 - msginChat.Value.text.Length * WIDTH_PER_CHAR, 0, 800), rt.sizeDelta.y); //AQUI ESTA EL CLAMP QUE NO CLAMPEA A 0
+                    msg.transform.Find("bg_image").GetComponent<RectTransform>().sizeDelta = new Vector2(-Mathf.Clamp(800 - msginChat.Value.text.Length * WIDTH_PER_CHAR, 0, 800), rt.sizeDelta.y);
                     msg.GetComponent<RectTransform>().sizeDelta = new Vector2(msg.GetComponent<RectTransform>().sizeDelta.x, 160 + HEIGHT_PER_LINE * cont_sl);
 
+                    //Answers containers
                     foreach (var ans in msginChat.Value.answers){
                         int cont_l = ans.Value.text.Length < 29 ? 0 : ans.Value.text.Length / 29 + 1;
                         your_msg.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = ans.Value.text;
 
                         GameObject msg_ans = Instantiate(your_msg, transform);
+                        RectTransform rt_ans = msg_ans.transform.Find("bg_image").GetComponent<RectTransform>();
+                        msg_ans.transform.Find("bg_image").GetComponent<RectTransform>().offsetMin = new Vector2(Mathf.Clamp(1200 - msginChat.Value.text.Length * 40, 453, 1200), rt_ans.offsetMin.y);
+                        print(Mathf.Clamp(1200 - msginChat.Value.text.Length * 40, 453, 1200));
                         msg_ans.GetComponent<RectTransform>().sizeDelta = new Vector2(msg_ans.GetComponent<RectTransform>().sizeDelta.x, 160 + HEIGHT_PER_LINE * cont_l);
                     }
                 }               
