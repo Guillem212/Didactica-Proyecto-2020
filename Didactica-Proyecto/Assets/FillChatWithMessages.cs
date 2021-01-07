f1649025e36fc71e9b93f2e7e3d0cf17fef8e880
riki8245 <56740262+riki8245@users.noreply.github.com>
2021-01-07 18:24:16 +0100

    changes in messagesFiller

--- Didactica-Proyecto/Assets/FillChatWithMessages.cs
+++ Didactica-Proyecto/Assets/FillChatWithMessages.cs
@@ -37,41 +37,47 @@ public class FillChatWithMessages : MonoBehaviour
             {
                 foreach (var msginChat in item.Value.messages)
                 {
-                    #region Adjust text to fit the container 
-                    int m_cont_sl = 0; //msginChat.Value.text.Length < 29? 0  : (int)(((float)msginChat.Value.text.Length / 29) - (((float)msginChat.Value.text.Length / 29) % 1));
-                    int m_cont_ch = 0;
-                    int m_cont = 0;
-                    string modified_msg = "";
-                    string m_aux = "";
-                    foreach(char c in msginChat.Value.text)
+                    if (msginChat.Value.isActive && msginChat.Value.text.Length > 0)
                     {
-                        m_cont++;
-                        m_cont_ch++;
-                        m_aux += c;
-                        if (c.Equals(' ') || msginChat.Value.text.Length == m_cont) { modified_msg += m_aux; m_aux = ""; }
-                        if (m_cont_ch > 28)
+                        #region Adjust text to fit the container 
+                        int m_cont_sl = 0;
+                        int m_cont_ch = 0;
+                        int m_cont = 0;
+                        string modified_msg = "";
+                        string m_aux = "";
+                        foreach (char c in msginChat.Value.text)
                         {
-                            modified_msg += m_aux.Length < 29 ? "\n" : "";
-                            m_aux += m_aux.Length < 29 ? "" : "\n";
-                            m_cont_ch = m_aux.Length < 29? m_aux.Length : 0;
-                            m_cont_sl++;
+                            m_cont++;
+                            m_cont_ch++;
+                            /*if (m_cont_ch == 29 && c.Equals(' ')) m_aux += "";
+                            else m_aux += c;*/
+                            m_aux += (m_cont_ch == 29 && c.Equals(' ')) ? "" : c.ToString();
+                            if (c.Equals(' ') || msginChat.Value.text.Length == m_cont) { modified_msg += m_aux; m_aux = ""; }
+                            if (m_cont_ch > 28)
+                            {
+                                modified_msg += m_aux.Length < 29 ? "\n" : "";
+                                m_aux += m_aux.Length < 29 ? "" : "\n";
+                                m_cont_ch = m_aux.Length < 29 ? m_aux.Equals("") ? 1 : m_aux.Length : 0;
+                                m_cont_sl += msginChat.Value.text.Length == m_cont  && m_aux.Equals("") ? 0 : 1;
+                            }
                         }
+                        #endregion
+
+                        received_msg.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = modified_msg;
+                        GameObject msg = Instantiate(received_msg, transform);
+
+                        RectTransform rt = msg.transform.Find("bg_image").GetComponent<RectTransform>();
+                        msg.transform.Find("bg_image").GetComponent<RectTransform>().sizeDelta = new Vector2(-Mathf.Clamp(800 - msginChat.Value.text.Length * WIDTH_PER_CHAR, 0, 800), rt.sizeDelta.y);
+                        msg.GetComponent<RectTransform>().sizeDelta = new Vector2(msg.GetComponent<RectTransform>().sizeDelta.x, 160 + HEIGHT_PER_LINE * m_cont_sl);
+
                     }
-                    #endregion
-
-                    received_msg.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = modified_msg;
-                    GameObject msg = Instantiate(received_msg, transform);
-
-                    RectTransform rt = msg.transform.Find("bg_image").GetComponent<RectTransform>();
-                    msg.transform.Find("bg_image").GetComponent<RectTransform>().sizeDelta = new Vector2(-Mathf.Clamp(800 - msginChat.Value.text.Length * WIDTH_PER_CHAR, 0, 800), rt.sizeDelta.y);
-                    msg.GetComponent<RectTransform>().sizeDelta = new Vector2(msg.GetComponent<RectTransform>().sizeDelta.x, 160 + HEIGHT_PER_LINE * m_cont_sl);
 
                     //Answers containers
                     foreach (var ans in msginChat.Value.answers){
-                        if (ans.Value.isSelected)
+                        if (ans.Value.isSelected && ans.Value.text.Length > 0)
                         {
                             #region Adjust text to fit the container
-                            int a_cont_sl = 0; // ans.Value.text.Length < 29 ? 0 : ans.Value.text.Length / 29 + 1;
+                            int a_cont_sl = 0;
                             int a_cont_ch = 0;
                             int a_cont = 0;
                             string modified_ans = "";
@@ -80,21 +86,24 @@ public class FillChatWithMessages : MonoBehaviour
                             {
                                 a_cont++;
                                 a_cont_ch++;
-                                a_aux += c;
+                                /*
+                                if (a_cont_ch == 29 && c.Equals(' ')) a_aux += "";
+                                else a_aux += c;*/
+                                a_aux += (a_cont_ch == 29 && c.Equals(' ')) ? "" : c.ToString();
                                 if (c.Equals(' ') || ans.Value.text.Length == a_cont) { modified_ans += a_aux; a_aux = ""; }
                                 if (a_cont_ch > 28)
                                 {
                                     modified_ans += a_aux.Length < 29 ? "\n" : "";
                                     a_aux += a_aux.Length < 29 ? "" : "\n";
-                                    a_cont_ch = a_aux.Length < 29 ? a_aux.Length : 0;
-                                    a_cont_sl++;
+                                    a_cont_ch = a_aux.Length < 29 ? a_aux.Equals("")? 1 : a_aux.Length : 0;
+                                    a_cont_sl += ans.Value.text.Length == a_cont && a_aux.Equals("") ? 0 : 1;
                                 }
                             }
                             #endregion
 
                             your_msg.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = modified_ans;
                             GameObject msg_ans = Instantiate(your_msg, transform);
-
+                            print(modified_ans);
                             RectTransform rt_ans = msg_ans.transform.Find("bg_image").GetComponent<RectTransform>();
                             msg_ans.transform.Find("bg_image").GetComponent<RectTransform>().offsetMin = new Vector2(Mathf.Clamp(1200 - ans.Value.text.Length * WIDTH_PER_CHAR, 453, 1200), rt_ans.offsetMin.y);
                             msg_ans.GetComponent<RectTransform>().sizeDelta = new Vector2(msg_ans.GetComponent<RectTransform>().sizeDelta.x, 160 + HEIGHT_PER_LINE * a_cont_sl);
